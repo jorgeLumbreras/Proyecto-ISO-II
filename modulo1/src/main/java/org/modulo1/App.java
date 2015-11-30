fe5027022ac808008040daaedbc46be75bed1e0b
sopas11 <jorges.jlr@outlook.es>
2015-11-30 18:39:10 +0100

    Creación del proyecto maven, estructura inicial

---
+++ modulo1/src/main/java/org/modulo1/App.java
@@ -0,0 +1,13 @@
+package org.modulo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
